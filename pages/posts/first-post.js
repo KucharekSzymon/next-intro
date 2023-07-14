ce8a0a32b2593fbb665c61d317f14e854f7224e9
Szymon Kucharek <skucharek2@gmail.com>
2023-07-14 17:39:26 +0200

    [+] Image asset

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from 'next/image';
 
 export default function FirstoPost(){
     return (
@@ -7,6 +8,14 @@ export default function FirstoPost(){
         <h2>
             <Link href="/">Back to home</Link>
         </h2>
+        <div>
+            <Image
+                src="/images/profile.jpg"
+                height="144"
+                width="144"
+                alt="Name image"
+            />
+        </div>
     </>
     )
 }
