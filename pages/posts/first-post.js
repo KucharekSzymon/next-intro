034d2032d637420397a28916f8fd8a6e56dba8c0
Szymon Kucharek <skucharek2@gmail.com>
2023-07-14 17:54:07 +0200

    [+] Scripts import

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Image from 'next/image';
 import Head from "next/head";
+import Script from 'next/script';
+
 
 export default function FirstoPost(){
     return (
@@ -8,6 +10,14 @@ export default function FirstoPost(){
         <Head>
             <title>Firts page</title>
         </Head>
+
+        <Script
+            src="https://connect.facebook.net/en_US/sdk.js"
+            strategy="lazyOnload"
+            onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+            }
+        />
 
         <h1>First post</h1>
 
