20456c8f6276f58c0dfdb34f39bcb0ef0b471899
Szymon Kucharek <skucharek2@gmail.com>
2023-07-14 17:48:51 +0200

    [+] Page header

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
-import { useState } from 'react';
+import Head from 'next/head';
 import Link from 'next/link';
 
 function Header({ title }) {
 return(
-  <h1 className="title">
-    Read <Link href="/posts/first-post">this page!</Link>
-  </h1>
+  <>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <h1 className="title">
+      Read <Link href="/posts/first-post">this page!</Link>
+    </h1>
+  </>
 ) 
 }
 
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import Image from 'next/image';
+import Head from "next/head";
 
 export default function FirstoPost(){
     return (
     <>
+    <Head>
+        <title>Firts page</title>
+    </Head>
         <h1>First post</h1>
         <h2>
             <Link href="/">Back to home</Link>
